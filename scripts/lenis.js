5f872280fd9035068ee58df8a4452b41d7737535
sherryjutt932 <ladlajutt442@gmail.com>
2023-11-11 21:31:06 +0500

    lenis latest

--- scripts/lenis.js
+++ scripts/lenis.js
@@ -5,7 +5,7 @@ lenis.on('scroll', ScrollTrigger.update)
 gsap.ticker.add((time)=>{
 
   if(isMobile()){
-    lenis.raf(time * 100)
+    lenis.raf(time * 300)
   }
   else{
     lenis.raf(time * 500)
